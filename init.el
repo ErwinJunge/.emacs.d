22b7f0f48b5c821a2f6da37c929eb6049b2a39cc
Erwin Junge <erwin@junge.nl>
2014-05-14 11:19:09 +0200

    Reset Custom

--- init.el
+++ init.el
@@ -102,15 +102,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(custom-safe-themes (quote ("f715f948867d85fa384b6c20d793dfd126d71996afd62f9d003705c960929977" default)))
- '(ecb-options-version "2.40")
+ '(custom-safe-themes (quote ("11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" default)))
  '(fci-rule-color "#383838")
- '(graphviz-dot-auto-indent-on-semi nil)
- '(graphviz-dot-preview-extension "png")
- '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
- '(help-at-pt-timer-delay 0.9)
- '(safe-local-variable-values (quote ((project-venv-name . "openict-erp") (project-venv-name . "django-rest-framework-test") (project-venv-name . "django-rest-framework") (project-venv-name . "pony") (project-venv-name . "qollap") (project-venv-name . "bacchi") (project-venv-name . "beebox") (project-venv-name . "projects") (project-venv-name . "myproject-env"))))
+ '(safe-local-variable-values (quote ((project-venv-name . "bacchi") (project-venv-name . "beebox") (project-venv-name . "qollap") (project-venv-name . "openict-erp"))))
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

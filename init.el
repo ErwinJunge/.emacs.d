4fd322abde3020ca32238bab9d5ba3ccd1821826
Erwin Junge <erwin@junge.nl>
2015-07-07 11:00:04 +0200

    Add default custom-set-faces

--- init.el
+++ init.el
@@ -287,3 +287,9 @@
  '(yas-indent-line (quote auto)))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

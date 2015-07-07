4b33ec22ebcd7c00d4b9c7ff4822eb0a2fa24015
Erwin Junge <erwin@junge.nl>
2015-07-07 11:00:12 +0200

    Update safe project-venv-name list

--- init.el
+++ init.el
@@ -234,7 +234,8 @@
  '(python-indent-offset 4)
  '(safe-local-variable-values
    (quote
-    ((project-venv-name . "coach360")
+    ((project-venv-name . "agriplace")
+     (project-venv-name . "coach360")
      (project-venv-name . "qollap_chat_kivy")
      (project-venv-name . "kivy")
      (project-venv-name . "brandfighters")

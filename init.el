efc6bb5431b0d20fc913532cda00a65c276e0ce9
Erwin Junge <erwin@junge.nl>
2016-05-13 10:17:39 +0200

    Filter slack bots

--- init.el
+++ init.el
@@ -259,6 +259,8 @@
   (interactive)
   (erc-tls :server "stamkracht.irc.slack.com" :port 6667 :nick "erwin" :password nil))
 
+(setq erc-ignore-list '("SK_Dashboard" "Beanstalk"))
+
 ;; Custom
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
